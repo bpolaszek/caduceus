e5f9733590a3df62c04059e9d8a213f07126dfe8
Beno!t POLASZEK <bpolaszek@gmail.com>
2025-05-14 15:19:28 +0200

    Initial set of files

---
+++ vite.config.js
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts'
+import path from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [dts()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'caduceus',
+      fileName: (format) => `caduceus.${format}.js`,
+    },
+    rollupOptions: {
+      // createEndpoints sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['buffer'],
+      output: {
+        exports: 'named',
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          buffer: 'buffer',
+        },
+      },
+    },
+  },
+});
